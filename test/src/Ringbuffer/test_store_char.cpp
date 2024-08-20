1d80a91a946bfcefd5c08d2de3a510623f7e5535
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2024-08-20 09:38:50 +0530

    Squashed 'cores/arduino/' content from commit 4a02bfc
    git-subtree-dir: cores/arduino
    git-subtree-split: 4a02bfc0a924e1fec34c3bb82ffd5dfba7643a0c

---
+++ test/src/Ringbuffer/test_store_char.cpp
@@ -0,0 +1,26 @@
+/*
+ * Copyright (c) 2020 Arduino.  All rights reserved.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+/**************************************************************************************
+ * INCLUDE
+ **************************************************************************************/
+
+#include <catch.hpp>
+
+#include <api/RingBuffer.h>
+
+/**************************************************************************************
+ * TEST CODE
+ **************************************************************************************/
+
+TEST_CASE ("Data is put into the ring buffer via 'store_char'", "[Ringbuffer-store_char-01]")
+{
+  arduino::RingBufferN<2> ringbuffer;
+  ringbuffer.store_char('A');
+  REQUIRE(ringbuffer._aucBuffer[0] == 'A');
+  ringbuffer.store_char('B');
+  REQUIRE(ringbuffer._aucBuffer[1] == 'B');
+}
---
+++ test/src/main.cpp
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2020 Arduino.  All rights reserved.
+ *
+ * SPDX-License-Identifier: LGPL-2.1-or-later
+ */
+
+/**************************************************************************************
+ * INCLUDE/MAIN
+ **************************************************************************************/
+
+#define CATCH_CONFIG_MAIN
+#include <catch.hpp>
