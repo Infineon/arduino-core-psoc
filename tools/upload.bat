3628952cd9d6e54da52242ea6ca942eda97b216f
jaenrig-ifx <enriquezgarcia.external@infineon.com>
2024-10-02 09:25:29 +0200

    tools/upload.bat: Added adapter .bat script for Windows.
    Signed-off-by: jaenrig-ifx <enriquezgarcia.external@infineon.com>

---
+++ tools/upload.bat
@@ -0,0 +1,33 @@
+@echo off
+setlocal enabledelayedexpansion
+
+
+REM Get the arguments passed in the command line
+
+REM Command
+set upload_cmd=%1
+
+REM Paths
+set mtb_tools_path=%2
+set platform_path=%3
+set build_path=%4
+
+REM Board parameters 
+set board_variant=%5
+set board_serial_num=%6
+set board_openocd_cfg=%7
+
+REM Project parameters
+set project_name=%8
+
+REM "Optional arguments"
+REM To access 10th argument, we need to shift
+set verbose_flag=%9
+
+REM Replace backslashes with forward slashes
+REM for openocd to work properly
+set "mtb_tools_path=!mtb_tools_path:\=/!"
+set "platform_path=!platform_path:\=/!"
+set "build_path=!build_path:\=/!"
+
+%mtb_tools_path%/modus-shell/bin/bash -i -l -c "bash %platform_path%/tools/upload.sh %upload_cmd% %mtb_tools_path% %platform_path% %build_path% %board_variant% %board_serial_num% %board_openocd_cfg% %project_name% %verbose_flag%"
