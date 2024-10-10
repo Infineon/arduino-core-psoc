9ffd864b9f529d2a1640709bb7ff6fe73a7d5e32
Ramya Subramanyam <ramya.subramanyam@infineon.com>
2024-10-10 12:59:13 +0530

    tools/ci.sh: Remove unshallow and run the verification for all commits.
    Signed-off-by: Ramya Subramanyam <ramya.subramanyam@infineon.com>

--- tools/ci.sh
+++ tools/ci.sh
@@ -30,8 +30,8 @@ function ci_commit_formatting_run {
     git remote add upstream https://github.com/Infineon/arduino-core-psoc.git
     git fetch --depth=100 upstream main
     # If the common ancestor commit hasn't been found, fetch more.
-    git merge-base upstream/main HEAD || git fetch --unshallow upstream main
+    git merge-base upstream/main HEAD || git fetch upstream main
     # For a PR, upstream/main..HEAD ends with a merge commit into main, exclude that one.
-    python tools/verifygitlog.py -n5 -v --no-merges
+    python tools/verifygitlog.py -v upstream/main..HEAD --no-merges
 }
 
